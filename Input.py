d1a2e99ba965f57c3ad208142e9db55d6d67f03c
Alofa-gif <80366499+Alofa-gif@users.noreply.github.com>
2021-05-06 14:55:56 +1200

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,9 @@
+name = input("What is your name")
+age = input("How old are you")
+address = input("What is your address")
+Country = input("What country do you come from or live in")
+
+
+print("here are your answers {} {} {} {} ".format(name, age, address, Country))
+print("do you to submit your answers")
+print("very well")
