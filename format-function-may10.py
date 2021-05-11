21aa8615a2d85c6f136deef493eff681d315ec99
Alofa-gif <80366499+Alofa-gif@users.noreply.github.com>
2021-05-11 12:07:29 +1200

    Update format-function-may10.py

--- format-function-may10.py
+++ format-function-may10.py
@@ -1,12 +1,14 @@
 name = input("What is your name?")
 surname = input("What is your surname")
 age = input("How old are you?")
-Nationality = input("What country are you legally authorized from")
+nationality = input("What country are you legally authorized from")
+address = input("What is your Address?")
+favouritefood = input("What is your favourite food to eat?")
 
-print("So information I have is {} {} {} {}".format(name, surname, age, Nationality))
-Correction = input("Do you wish to restart your answer you have answered with say something other than a word is same as yes, or say yes to stay with answers.")
+print("So information I have is {}, {}, {}, {}, {}, {}".format(name,surname,age,nationality,address,favouritefood))
+correction = input("Do you wish to restart your answer you have answered with say something other than a word is same as yes, or say yes to stay with your answers.")
 
-if Correction == "yes":
+if correction == "yes":
   print("very well then")
 else:
   print("Alright so to restart you either refresh the page or press ok")
